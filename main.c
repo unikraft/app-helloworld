e2b538c5566a7899ea39309f5d00403d22f2ea77
Simon Kuenzer <simon.kuenzer@neclab.eu>
2020-03-19 16:19:58 +0100

    Add option to stay alive
    Adds an option that does not shutdown the guest. When the boot process
    is done, an ASCII animation of a swimming fish is shown. This option
    can be useful (and entertaining) for demo purposes. The example can
    also be handy to test initial ports to new platforms or libcs.
    
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>
    Reviewed-by: Alexander Jung <a.jung@lancs.ac.uk>

--- main.c
+++ main.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <uk/essentials.h>
 
 /* Import user configuration: */
 #include <uk/config.h>
 
+#if CONFIG_APPHELLOWORLD_SPINNER
+static const char *spinner[] = {
+	">))'>        ",
+	" >))'>       ",
+	"  >))'>      ",
+	"   >))'>     ",
+	"    >))'o    ",
+	"     >))'>°  ",
+	"     <'((< ° ",
+	"    <'((<   '",
+	"   <'((<     ",
+	"  <'((<      ",
+	" <'((<       ",
+	"<'((<        ",
+};
+#endif
+
 int main(int argc, char *argv[])
 {
+#if CONFIG_APPHELLOWORLD_PRINTARGS || CONFIG_APPHELLOWORLD_SPINNER
+	int i;
+#endif
 	printf("Hello world!\n");
 
 #if CONFIG_APPHELLOWORLD_PRINTARGS
-	int i;
-
 	printf("Arguments: ");
 	for (i=0; i<argc; ++i)
 		printf(" \"%s\"", argv[i]);
 	printf("\n");
+#endif
+
+#if CONFIG_APPHELLOWORLD_SPINNER
+	i = 0;
+	printf("\n");
+	while (1) {
+		i %= ARRAY_SIZE(spinner);
+		printf("\r%s", spinner[i++]);
+		sleep(1);
+	}
 #endif
 }
