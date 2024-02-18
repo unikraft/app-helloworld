756542f9b0c83643a532b1e22a6910d63a63e522
Teodor Teugea <ioan_teodor.teugea@stud.acs.upb.ro>
2024-02-18 16:12:43 +0200

    basic somewhat working example of CET

--- main.c
+++ main.c
@@ -38,11 +38,22 @@ void cr4_enable_cet() {
 }
 
 void* create_shstk(size_t size) {
-	void *mem = mmap(NULL, size, PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	void *mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	if (mem == MAP_FAILED) {
 		printf("map failed\n");
 		return NULL;
 	}
+
+	// seems like the memory needs to actually be allocated (or there is some fucky wucky with the fault handling)
+	for (int i = 0; i < size; i++) {
+		((char*)mem)[i] = 0;
+	}
+
+	// create a restore token
+	void *stack_start = ((char*)mem) + size - PAGE_SIZE;
+	*(unsigned long long*)stack_start = (unsigned long long) stack_start;
+
+	// set the shadow stack page attributes
 	struct uk_vas* vas = uk_vas_get_active();
 	if (vas == NULL) {
 		printf("failed to get vas\n");
@@ -56,10 +67,13 @@ void* create_shstk(size_t size) {
 	return mem;
 }
 
+// the win function, shows the flag
 void g() {
 	printf("flag: ma-ta\n");
 }
 
+// has buffer overflow that could be used to call g
+// currently fopen does not work for some reason
 void f() {
 	char a[32];
 	FILE *fd = fopen("payload.bin", "rb");
@@ -81,11 +95,32 @@ void f() {
 	printf("read: %s\n", a);
 }
 
+
+// c and fact are used to make sure
+// the recursive calls are actually done as such and
+// the compiler does not optimise them away.
+// Checked with godbolt, SHOULD generate recursive calls.
+int c() {
+    int a = 5;
+    for (int i = 0; i < 1000; i++) {
+        a &= i;
+    }
+    return (a & 25);
+}
+
+
+int fact(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return fact(n - 1) + fact(n - 2) + fact(n - 3);
+}
+
 int main()
 {
 	void *shstk;
 
-	size_t shstk_len = 12 * PAGE_SIZE;
+	size_t shstk_len = 128 * PAGE_SIZE;
 
 	cr4_enable_cet();
 
@@ -95,13 +130,25 @@ int main()
 		return 1;
 	}
 
-	wrmsrl(MSR_IA32_PL0_SSP, shstk + shstk_len - PAGE_SIZE);
+	wrmsrl(MSR_IA32_PL0_SSP, (unsigned long long)((char*)shstk + shstk_len - PAGE_SIZE));
 	wrmsrl(MSR_IA32_S_CET, CET_SHSTK_EN);
+	asm volatile ("setssbsy" : : :); // apparently, for supervisor shadow stacks, this instruction needs to be called
+					 // and a "restore token" set in the shadow stack
 
-	printf("Hello world!\n");
+	void *ssp = 0x1;
+	asm volatile ("rdsspq %0" : "=r"(ssp) : : );
+	
+	int n = fact(c() + 12);
+	printf("SSP: %p recursive: %d\n", ssp, n);
+//	FILE *f = fopen("payload.bin", "rb"); - does not work, need to check why
+//	if (f == NULL) {
+//		printf("file ptr is null\n");
+//	}
+	//g();
+	//f();
 
-//	f();
-
+	// asm volatile ("clrssbsy %0" : : "r"((unsigned long long)ssp) :); -- the opposite of setssbsy, looks like the code runs fine without it
+									    // still, should try to get this working
 	wrmsrl(MSR_IA32_S_CET, 0);
 	wrmsrl(MSR_IA32_PL0_SSP, 0);
 
